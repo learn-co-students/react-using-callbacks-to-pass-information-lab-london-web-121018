5f018071ef74b411475a8fade5f0422ffd221361
github username <github email address>
2019-02-20 11:01:54 +0000

    Done.

--- src/Matrix.js
+++ src/Matrix.js
@@ -5,23 +5,41 @@ import ColorSelector from './ColorSelector.js'
 
 export default class Matrix extends Component {
 
-  constructor() {
-    super()
+ 
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      selectedColor: ''
+    }
   }
 
+  selectAColour = (e) => {
+    return this.setState({selectedColor: e.target.id})
+  }
+
+  changeCellColour= ()=> {
+      return this.state.selectedColor;
+  }
+
+  
+
+
   genRow = (vals) => (
-    vals.map((val, idx) => <Cell key={idx} color={val} />)
+    vals.map((val, idx) => <Cell key={idx} color={val} selectedColor={this.state.selectedColor}/>)
   )
 
   genMatrix = () => (
     this.props.values.map((rowVals, idx) => <div key={idx} className="row">{this.genRow(rowVals)}</div>)
   )
 
+  
+
 
   render() {
     return (
       <div id="app">
-        <ColorSelector />
+        <ColorSelector selectAColour={this.selectAColour}/>
         <div id="matrix">
           {this.genMatrix()}
         </div>
